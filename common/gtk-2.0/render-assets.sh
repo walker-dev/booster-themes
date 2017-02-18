220653192fae9b70a544088506882f78c19ba57a
Unknown <booster.devel@gmail.com>
2017-02-18 21:55:51 +0100

    Initial commit

--- common/gtk-2.0/render-assets.sh
+++ common/gtk-2.0/render-assets.sh
@@ -3,58 +3,54 @@
 INKSCAPE="/usr/bin/inkscape"
 OPTIPNG="/usr/bin/optipng"
 
-SRC_FILE="assets.svg"
-ASSETS_DIR="assets"
-
-DARK_SRC_FILE="assets-dark.svg"
-DARK_ASSETS_DIR="assets-dark"
-
+HOTROD_SRC_FILE="assets-hotrod.svg"
+HOTROD_ASSETS_DIR="assets-hotrod"
+NVIDIA_SRC_FILE="assets-nvidia.svg"
+NVIDIA_ASSETS_DIR="assets-nvidia"
 INDEX="assets.txt"
 
 for i in `cat $INDEX`
 do
 
-if [ -f $ASSETS_DIR/$i.png ]; then
-    echo $ASSETS_DIR/$i.png exists.
+if [ -f $HOTROD_ASSETS_DIR/$i.png ]; then
+    echo $HOTROD_ASSETS_DIR/$i.png exists.
 else
-    echo
-    echo Rendering $ASSETS_DIR/$i.png
+    echo Rendering $HOTROD_ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
               --export-id-only \
-              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
+              --export-png=$HOTROD_ASSETS_DIR/$i.png $HOTROD_SRC_FILE >/dev/null \
+    && $OPTIPNG -o7 --quiet $HOTROD_ASSETS_DIR/$i.png
 fi
 
-if [ -f $DARK_ASSETS_DIR/$i.png ]; then
-    echo $DARK_ASSETS_DIR/$i.png exists.
+if [ -f $NVIDIA_ASSETS_DIR/$i.png ]; then
+    echo $NVIDIA_ASSETS_DIR/$i.png exists.
 else
-    echo
-    echo Rendering $DARK_ASSETS_DIR/$i.png
+    echo Rendering $NVIDIA_ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
               --export-id-only \
-              --export-png=$DARK_ASSETS_DIR/$i.png $DARK_SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $DARK_ASSETS_DIR/$i.png 
+              --export-png=$NVIDIA_ASSETS_DIR/$i.png $NVIDIA_SRC_FILE >/dev/null \
+    && $OPTIPNG -o7 --quiet $NVIDIA_ASSETS_DIR/$i.png
 fi
 done
 
-cp $ASSETS_DIR/entry-toolbar.png menubar-toolbar/entry-toolbar.png
-cp $ASSETS_DIR/entry-active-toolbar.png menubar-toolbar/entry-active-toolbar.png
-cp $ASSETS_DIR/entry-disabled-toolbar.png menubar-toolbar/entry-disabled-toolbar.png
+cp $HOTROD_ASSETS_DIR/entry-toolbar.png menubar-toolbar/entry-toolbar.png
+cp $HOTROD_ASSETS_DIR/entry-active-toolbar.png menubar-toolbar/entry-active-toolbar.png
+cp $HOTROD_ASSETS_DIR/entry-disabled-toolbar.png menubar-toolbar/entry-disabled-toolbar.png
 
-cp $ASSETS_DIR/menubar.png menubar-toolbar/menubar.png
-cp $ASSETS_DIR/menubar_button.png menubar-toolbar/menubar_button.png
+cp $HOTROD_ASSETS_DIR/menubar.png menubar-toolbar/menubar.png
+cp $HOTROD_ASSETS_DIR/menubar_button.png menubar-toolbar/menubar_button.png
 
 
-cp $DARK_ASSETS_DIR/button.png menubar-toolbar/button.png
-cp $DARK_ASSETS_DIR/button-hover.png menubar-toolbar/button-hover.png
-cp $DARK_ASSETS_DIR/button-active.png menubar-toolbar/button-active.png
-cp $DARK_ASSETS_DIR/button-insensitive.png menubar-toolbar/button-insensitive.png
+cp $NVIDIA_ASSETS_DIR/button.png menubar-toolbar/button.png
+cp $NVIDIA_ASSETS_DIR/button-hover.png menubar-toolbar/button-hover.png
+cp $NVIDIA_ASSETS_DIR/button-active.png menubar-toolbar/button-active.png
+cp $NVIDIA_ASSETS_DIR/button-insensitive.png menubar-toolbar/button-insensitive.png
 
-cp $DARK_ASSETS_DIR/entry-toolbar.png menubar-toolbar/entry-toolbar-dark.png
-cp $DARK_ASSETS_DIR/entry-active-toolbar.png menubar-toolbar/entry-active-toolbar-dark.png
-cp $DARK_ASSETS_DIR/entry-disabled-toolbar.png menubar-toolbar/entry-disabled-toolbar-dark.png
+cp $NVIDIA_ASSETS_DIR/entry-toolbar.png menubar-toolbar/entry-toolbar-dark.png
+cp $NVIDIA_ASSETS_DIR/entry-active-toolbar.png menubar-toolbar/entry-active-toolbar-dark.png
+cp $NVIDIA_ASSETS_DIR/entry-disabled-toolbar.png menubar-toolbar/entry-disabled-toolbar-dark.png
 
-cp $DARK_ASSETS_DIR/menubar.png menubar-toolbar/menubar-dark.png
-cp $DARK_ASSETS_DIR/menubar_button.png menubar-toolbar/menubar_button-dark.png
+cp $NVIDIA_ASSETS_DIR/menubar.png menubar-toolbar/menubar-dark.png
+cp $NVIDIA_ASSETS_DIR/menubar_button.png menubar-toolbar/menubar_button-dark.png
 
 exit 0
--- common/gtk-3.0/3.22/render-assets.sh
+++ common/gtk-3.0/3.22/render-assets.sh
@@ -3,32 +3,52 @@
 INKSCAPE="/usr/bin/inkscape"
 OPTIPNG="/usr/bin/optipng"
 
-SRC_FILE="assets.svg"
-ASSETS_DIR="assets"
+HOTROD_SRC_FILE="assets-hotrod.svg"
+HOTROD_ASSETS_DIR="assets-hotrod"
+NVIDIA_SRC_FILE="assets-nvidia.svg"
+NVIDIA_ASSETS_DIR="assets-nvidia"
 INDEX="assets.txt"
 
 for i in `cat $INDEX`
-do 
-if [ -f $ASSETS_DIR/$i.png ]; then
-    echo $ASSETS_DIR/$i.png exists.
-else
-    echo
-    echo Rendering $ASSETS_DIR/$i.png
+do
+  if [ -f $HOTROD_ASSETS_DIR/$i.png ]; then
+    echo $HOTROD_ASSETS_DIR/$i.png exists.
+  else
+    echo Rendering $HOTROD_ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
-              --export-id-only \
-              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
-fi
-if [ -f $ASSETS_DIR/$i@2.png ]; then
-    echo $ASSETS_DIR/$i@2.png exists.
-else
-    echo
-    echo Rendering $ASSETS_DIR/$i@2.png
+    --export-id-only \
+    --export-png=$HOTROD_ASSETS_DIR/$i.png $HOTROD_SRC_FILE >/dev/null \
+    && $OPTIPNG -o7 --quiet $HOTROD_ASSETS_DIR/$i.png
+  fi
+  if [ -f $HOTROD_ASSETS_DIR/$i@2.png ]; then
+    echo $HOTROD_ASSETS_DIR/$i@2.png exists.
+  else
+    echo Rendering $HOTROD_ASSETS_DIR/$i@2.png
     $INKSCAPE --export-id=$i \
-              --export-dpi=180 \
-              --export-id-only \
-              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png 
-fi
+    --export-dpi=180 \
+    --export-id-only \
+    --export-png=$HOTROD_ASSETS_DIR/$i@2.png $HOTROD_SRC_FILE >/dev/null \
+    && $OPTIPNG -o7 --quiet $HOTROD_ASSETS_DIR/$i@2.png
+  fi
+
+  if [ -f $NVIDIA_ASSETS_DIR/$i.png ]; then
+    echo $NVIDIA_ASSETS_DIR/$i.png exists.
+  else
+    echo Rendering $NVIDIA_ASSETS_DIR/$i.png
+    $INKSCAPE --export-id=$i \
+    --export-id-only \
+    --export-png=$NVIDIA_ASSETS_DIR/$i.png $NVIDIA_SRC_FILE >/dev/null \
+    && $OPTIPNG -o7 --quiet $NVIDIA_ASSETS_DIR/$i.png
+  fi
+  if [ -f $NVIDIA_ASSETS_DIR/$i@2.png ]; then
+    echo $NVIDIA_ASSETS_DIR/$i@2.png exists.
+  else
+    echo Rendering $NVIDIA_ASSETS_DIR/$i@2.png
+    $INKSCAPE --export-id=$i \
+    --export-dpi=180 \
+    --export-id-only \
+    --export-png=$NVIDIA_ASSETS_DIR/$i@2.png $NVIDIA_SRC_FILE >/dev/null \
+    && $OPTIPNG -o7 --quiet $NVIDIA_ASSETS_DIR/$i@2.png
+  fi
 done
 exit 0
